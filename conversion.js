96100f14816fa045284ce3bca737282d720f16d2
Luiz <gustavoooliveira111@gmail.com>
2022-03-27 16:30:57 -0300

    initial commit

---
+++ conversion.js
@@ -0,0 +1,40 @@
+function conversion() {
+
+  let F
+  let C
+  let option
+  
+  while(option != 1 && option != 2){
+    
+    option = Number(prompt('Qual tipo de temperatura dejesa converter?\n1-Celsius\n2-Fahrenheit'))
+  }
+
+  switch(option){
+    case 1:
+      
+      C = Number(prompt('Digite a temperatura em Celsius'))
+
+      F = C * 9/5 + 32
+
+      alert(`${C}° graus Celsius convertidos em Fahrenheit são ${F.toFixed(2)}`)
+
+    break;
+
+    case 2:
+      
+      F = Number(prompt('Digite a temperatura em Fahrenheits'))
+      
+      C = (F - 32) * 5/9
+      
+      alert(`${F}° graus Fahrenheit convertidos em Celsius são ${C.toFixed
+      (2)}`)
+   
+    break;
+
+    default:
+      
+      alert('Opção inválida.')
+  } 
+}
+
+conversion()
