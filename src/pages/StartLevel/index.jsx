1cf47759163aca2de7206fea1f297511fe2c1c35
Luiz <gustavoooliveira111@gmail.com>
2023-03-02 16:11:10 -0300

    adding text dynamically and handling errors

--- src/pages/StartLevel/index.jsx
+++ src/pages/StartLevel/index.jsx
@@ -1,17 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./styles.css";
 
 export const StartLevel = () => {
-  
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const levelText = {
+    1: "know about me", 
+    2: "explore my skills", 
+    3: "explore my projects",
+    4: "explore my work journey"
+  }
 
   return (
     <div className="start-level-wrapper">
-      <h1 className="double-color">Level {id}</h1>
-      <h2>know about me</h2>
-      <button>
+      <h1 className="double-color">Level {id}</h1>  
+      <h2>{levelText[id] ? levelText[id] : "level not found"}</h2>
+      <button onClick={() => {
+        levelText[id] ? console.log("right") : navigate("/", {replace: true});
+      }}>
         <span className="double-color">
-          start
+          {levelText[id] ? "start" : "back"}
         </span>
       </button>
     </div>
